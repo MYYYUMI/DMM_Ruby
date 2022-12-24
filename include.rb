4ad0d9fa8d443dac874732933380d2bc7b18540e
EC2 Default User <ec2-user@ip-172-31-16-193.ap-northeast-1.compute.internal>
2022-12-24 04:09:57 +0000

    [Finish] 2-7 例題作成

---
+++ include.rb
@@ -0,0 +1,5 @@
+puts "WEBCAMPでプログラム学習".include?("WEBCAMP")
+# 引数に指定した文字列が含まれているかどうかを調べるinclude?メソッド
+# 「WEBCAMPでプログラミング学習」の中には「WEBCAMP」が含まれているため、
+# 正しいことを表すtrueが出力される。
+# 含まれていない場合はfalseが出力される。
---
+++ length.rb
@@ -0,0 +1,2 @@
+puts "WEBCAMP".length
+#「WEBCAMPの文字列」を「lengthメソッドで文字数を数えて」、「putsメソッドで表示する」
